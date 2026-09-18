1adfec61debee4043e19ce796618d902b1a285c5
Krish <krishcshah09@gmail.com>
2026-09-18 13:30:28 +0530

    finally and user defined exception

---
+++ FinallyDemo.java
@@ -0,0 +1,36 @@
+class FinallyDemo{
+	static void procA(){
+		try{
+			System.out.println("Inside procA");
+			throw new RuntimeException("demo");
+		}/*catch(Exception e){
+			System.out.println("Inside catch of procA");
+		}*/finally{
+			System.out.println("procA finally");
+		}
+	}
+	static void procB(){
+		try{
+			System.out.println("Inside procB");
+			return;
+		}finally{
+			System.out.println("procB finally");
+		}
+	}
+	static void procC(){
+		try{
+			System.out.println("Inside procC");
+		}finally{
+			System.out.println("procC finally");
+		}
+	}
+	public static void main(String[] args) {
+		try{
+			procA();
+		}catch(Exception e){
+			System.out.println("Exception caught");
+		}
+		procB();
+		procC();
+	}
+}
